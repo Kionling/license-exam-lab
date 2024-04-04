4e14bfca224c661b873c751ae23c9069f93092af
Daniel Jauregui <djvdjv209@gmail.com>
2024-04-03 22:48:20 -0700

    Defined function that checks for student answers

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def get_student_answers():
+def getStudentAnswers():
     student_answers = []
     for i in range(1, 21):
         answer = 'A'  
@@ -7,3 +7,17 @@ def get_student_answers():
             answer = 'A'  
         student_answers.append(answer)
     return student_answers
+
+
+
+def check_passed(student_answers):
+    correct_answers = ['B', 'D', 'A', 'A', 'C', 'A', 'B', 'A', 'C', 'D', 'B', 'C', 'D', 'A', 'D', 'C', 'C', 'B', 'D', 'A']
+    count_correct = 0
+    missed_questions = []
+    for i in range(len(student_answers)):
+        if student_answers[i] == correct_answers[i]:
+            count_correct += 1
+        else:
+            missed_questions.append(i + 1)
+    passed = count_correct >= 15
+    return passed, count_correct, missed_questions
